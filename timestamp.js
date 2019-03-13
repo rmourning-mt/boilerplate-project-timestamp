c295e83497d8fdd1fb0b903d240fc88a63daaaf2
Robert Mourning <rmourning@milestechnologies.com>
2019-03-13 11:28:45 -0400

    fix logic for checking whether date_string is invalid

--- timestamp.js
+++ timestamp.js
@@ -1,15 +1,19 @@
 function getDateJson(value) {
-    var date = value ? new Date(value) : new Date();
-    try {
-        return {
-            "unix": date.getTime(),
-            "utc": date.toUTCString()
-        };
-    } catch (ex) {
-        return {
-            "error": "Invalid Date"
-        };
+    var date = value ? parseDateString(value) : new Date();
+    return date ? {
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+    } : {
+        "error": "Invalid Date"
+    };
+}
+
+function parseDateString (value) {
+    var timestamp = Date.parse(value);
+    if (isNaN(timestamp)) {
+        return null;
     }
+    return new Date(timestamp);
 }
 
 module.exports = {
